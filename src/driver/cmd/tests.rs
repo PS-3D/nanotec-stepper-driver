15b6bdafb5b69057bd8e3360bfde02893ce86561
Maximilian Fischer <mfischer@ps-3d.de>
2022-06-10 18:36:45 +0200

    Remade cmd tests

--- src/driver/cmd/tests.rs
+++ src/driver/cmd/tests.rs
@@ -1,101 +1,206 @@
-mod payload {
-    use super::super::{MotorStop, Payload};
+use nom::Finish;
 
-    #[test]
-    fn parse_standalone() {
-        let (remainder, res) = Payload::parse(b"A").unwrap();
-        assert!(remainder.len() == 0);
-        assert_eq!(res, Payload::StartMotor);
-    }
+use super::{MotorStop, MotorType, Msg, PositioningMode, Record, RespondMode, RotationDirection};
 
-    #[test]
-    fn parse_subparser() {
-        let (remainder, res) = Payload::parse(b"S1").unwrap();
-        assert!(remainder.len() == 0);
-        assert_eq!(res, Payload::StopMotor(MotorStop::BrakeRamp));
-    }
-
-    #[test]
-    fn parse_read_subparser() {
-        let (remainder, res) = Payload::parse(b"Zu133742").unwrap();
-        assert!(remainder.len() == 0);
-        assert_eq!(res, Payload::MinFrequency(Some(133742)));
-    }
-
-    #[test]
-    #[should_panic]
-    fn parse_invalid_key() {
-        let (_, _) = Payload::parse(b"some_key_that_doesn't_exist133742").unwrap();
-    }
-
-    #[test]
-    #[should_panic]
-    fn parse_read_invalid_key() {
-        let (_, _) = Payload::parse(b"Zsome_key_that_doesn't_exist133742").unwrap();
-    }
-
-    #[test]
-    #[should_panic]
-    fn parse_missing_value() {
-        let (_, _) = Payload::parse(b"S").unwrap();
-    }
-
-    #[test]
-    #[should_panic]
-    fn parse_read_missing_value() {
-        let (_, _) = Payload::parse(b"Zo").unwrap();
-    }
-
-    #[test]
-    fn parse_rotation_direction_change() {
-        let (remainder, res) = Payload::parse(b"t1").unwrap();
-        assert!(remainder.len() == 0);
-        assert_eq!(res, Payload::RotationDirectionChange(Some(true)));
-    }
-
-    #[test]
-    fn fmt_standalone() {
-        let pl = Payload::StartMotor;
-        assert_eq!(format!("{}", pl), "A");
-    }
-
-    #[test]
-    fn fmt_with_value() {
-        let pl = Payload::StopMotor(MotorStop::BrakeRamp);
-        assert_eq!(format!("{}", pl), "S1");
-    }
-
-    #[test]
-    fn fmt_write() {
-        let pl = Payload::TravelDistance(Some(-1337));
-        assert_eq!(format!("{}", pl), "s-1337");
-    }
-
-    #[test]
-    fn fmt_read() {
-        let pl = Payload::MinFrequency(None);
-        assert_eq!(format!("{}", pl), "Zu");
-    }
+#[test]
+fn motortype_parse() {
+    let (_, t) = MotorType::parse(b"+0").finish().unwrap();
+    let expected = MotorType::Stepper;
+    assert_eq!(t, expected)
 }
 
-mod cmd {
-    use super::super::{Cmd, Payload};
+#[test]
+#[should_panic]
+fn motortype_parse_oob() {
+    let (_, _) = MotorType::parse(b"+3").finish().unwrap();
+}
 
-    #[test]
-    fn fmt_single() {
-        let cmd = Cmd {
-            address: Some(42),
-            payload: Payload::StartMotor,
-        };
-        assert_eq!(format!("{}", cmd), "#42A\r");
-    }
+#[test]
+#[should_panic]
+fn motortype_parse_garbage() {
+    let (_, _) = MotorType::parse(b"asdf").finish().unwrap();
+}
 
-    #[test]
-    fn fmt_all() {
-        let cmd = Cmd {
-            address: None,
-            payload: Payload::StartMotor,
-        };
-        assert_eq!(format!("{}", cmd), "#*A\r")
-    }
+#[test]
+fn motortype_display() {
+    let s = format!("{}", MotorType::BLDCHall);
+    let expected = "1";
+    assert_eq!(expected, s)
+}
+
+#[test]
+fn motorstop_display() {
+    let s = format!("{}", MotorStop::QuickStop);
+    let expected = "0";
+    assert_eq!(expected, s)
+}
+
+#[test]
+fn respondmode_parse() {
+    let (_, m) = RespondMode::parse(b"+0").finish().unwrap();
+    let expected = RespondMode::Quiet;
+    assert_eq!(m, expected)
+}
+
+#[test]
+#[should_panic]
+fn respondmode_parse_oob() {
+    let (_, _) = RespondMode::parse(b"+3").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn respondmode_parse_garbage() {
+    let (_, _) = RespondMode::parse(b"asdf").finish().unwrap();
+}
+
+#[test]
+fn respondmode_display() {
+    let s = format!("{}", RespondMode::NotQuiet);
+    let expected = "1";
+    assert_eq!(expected, s)
+}
+
+#[test]
+fn positioningmode_parse() {
+    let (_, m) = PositioningMode::parse(b"+7").finish().unwrap();
+    let expected = PositioningMode::ClockDirectionManualLeft;
+    assert_eq!(m, expected)
+}
+
+#[test]
+#[should_panic]
+fn positioningmode_parse_oob() {
+    let (_, _) = PositioningMode::parse(b"+0").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn positioningmode_parse_garbage() {
+    let (_, _) = PositioningMode::parse(b"asdf").finish().unwrap();
+}
+
+#[test]
+fn positioningmode_display() {
+    let s = format!("{}", PositioningMode::Absolute);
+    let expected = "2";
+    assert_eq!(expected, s)
+}
+
+#[test]
+fn rotationdirection_parse() {
+    let (_, d) = RotationDirection::parse(b"+1").finish().unwrap();
+    let expected = RotationDirection::Right;
+    assert_eq!(d, expected)
+}
+
+#[test]
+#[should_panic]
+fn rotationdirection_parse_oob() {
+    let (_, _) = RotationDirection::parse(b"+2").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn rotationdirection_parse_garbage() {
+    let (_, _) = RotationDirection::parse(b"asdf").finish().unwrap();
+}
+
+#[test]
+fn rotationdirection_display() {
+    let s = format!("{}", RotationDirection::Left);
+    let expected = "0";
+    assert_eq!(expected, s)
+}
+
+#[test]
+fn record_parse() {
+    let (_, r) = Record::parse(b"p+1s+400u+400o+1000n+1000b+2364B+0d+0t+0W+1P+0N+0:b+1:B+0")
+        .finish()
+        .unwrap();
+    let expected = Record {
+        positioning_mode: PositioningMode::Relative,
+        travel_distance: 400,
+        min_frequency: 400,
+        max_frequency: 1000,
+        max_frequency2: 1000,
+        accel_ramp: 2364,
+        brake_ramp: 0,
+        rotation_direction: RotationDirection::Left,
+        rotation_direction_change: false,
+        repetitions: 1,
+        record_pause: 0,
+        continuation_record: 0,
+    };
+    assert_eq!(r, expected);
+}
+
+#[test]
+fn msg_parse_long_write() {
+    let (_, m) = Msg::parse(b"1:Keyword=1337\r").finish().unwrap();
+    let expected = Msg {
+        address: Some(1),
+        payload: b":Keyword=1337".to_vec(),
+    };
+    assert_eq!(m, expected)
+}
+
+#[test]
+fn msg_parse_long_read() {
+    let (_, m) = Msg::parse(b"1:CL_motor_type+1\r").finish().unwrap();
+    let expected = Msg {
+        address: Some(1),
+        payload: b":CL_motor_type+1".to_vec(),
+    };
+    assert_eq!(m, expected)
+}
+
+#[test]
+fn msg_parse_short_no_arg() {
+    let (_, m) = Msg::parse(b"1A\r").finish().unwrap();
+    let expected = Msg {
+        address: Some(1),
+        payload: b"A".to_vec(),
+    };
+    assert_eq!(m, expected)
+}
+
+#[test]
+fn msg_parse_all() {
+    let (_, m) = Msg::parse(b"*S+1\r").finish().unwrap();
+    let expected = Msg {
+        address: None,
+        payload: b"S+1".to_vec(),
+    };
+    assert_eq!(m, expected)
+}
+
+#[test]
+#[should_panic]
+fn msg_parse_no_return() {
+    let (_, _) = Msg::parse(b"*S+1").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn msg_parse_no_addr() {
+    let (_, _) = Msg::parse(b"S+1\r").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn msg_parse_addr_oob() {
+    let (_, _) = Msg::parse(b"1337S+1\r").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn msg_parse_no_payload() {
+    let (_, _) = Msg::parse(b"1\r").finish().unwrap();
+}
+
+#[test]
+#[should_panic]
+fn msg_parse_garbage() {
+    let (_, _) = Msg::parse(b"gsrceitng").finish().unwrap();
 }
