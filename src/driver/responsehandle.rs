b077a7d858198810eb8c0a8060a62f13f070152e
Maximilian Fischer <mfischer@ps-3d.de>
2022-07-15 23:33:17 +0200

    Added recoverable errors for responsehandle.wait

--- src/driver/responsehandle.rs
+++ src/driver/responsehandle.rs
@@ -1,12 +1,12 @@
 use super::{cmd::frame::MotorAddress, DriverError, InnerDriver};
 use std::{
     cell::RefCell,
+    fmt::Debug,
     io::{Read, Write},
     marker::PhantomData,
     rc::Rc,
 };
-
-// TODO maybe wait macro?
+use thiserror::Error;
 
 // unfortunately, due to rustfmt not having the blank_lines_upper_bound feature
 // stable yet, we gotta put comments in between the different sections. otherwise
@@ -14,9 +14,96 @@ use std::{
 
 //
 
+/// Holds a recoverable DriverError
+#[derive(Error)]
+pub struct RecoverableResponseError<H: ResponseHandle<T>, T> {
+    pub handle: H,
+    pub error: DriverError,
+    pub marker: PhantomData<T>,
+}
+
+impl<H: ResponseHandle<T>, T> RecoverableResponseError<H, T> {
+    // maps the handle to anther handle using f
+    fn map_handle<O, F>(self, f: F) -> RecoverableResponseError<O, T>
+    where
+        F: FnOnce(H) -> O,
+        O: ResponseHandle<T>,
+    {
+        RecoverableResponseError {
+            handle: f(self.handle),
+            error: self.error,
+            marker: PhantomData,
+        }
+    }
+}
+
+// have to do it manually because we don't really want ResponseHandle to require
+// Debug
+impl<H: ResponseHandle<T>, T> Debug for RecoverableResponseError<H, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(&self.error, f)
+    }
+}
+
+//
+
+/// Error that contains ResponseHandle if error is recoverable.
+///
+/// Some DriverErrors are still recoverable. For example for
+/// [`DriverError::UnexpectedResponse`], we can still continue to wait for the
+/// actual result. For that, [`RecoverableResponseError`] contains another
+/// ResponseHandle that waits for the result, as well as the original error.
+#[derive(Error)]
+pub enum ResponseError<H: ResponseHandle<T>, T> {
+    #[error(transparent)]
+    Recoverable(RecoverableResponseError<H, T>),
+    #[error(transparent)]
+    Fatal(DriverError),
+}
+
+impl<H: ResponseHandle<T>, T> ResponseError<H, T> {
+    fn from_driver_error(handle: H, error: DriverError) -> Self {
+        let error = match error {
+            DriverError::UnexpectedResponse(r) => DriverError::UnexpectedResponse(r),
+            _ => return Self::Fatal(error),
+        };
+        Self::Recoverable(RecoverableResponseError {
+            handle,
+            error,
+            marker: PhantomData,
+        })
+    }
+
+    // maps the handle of RecoverableResponseERror to another handle using f
+    fn map_handle<O, F>(self, f: F) -> ResponseError<O, T>
+    where
+        F: FnOnce(H) -> O,
+        O: ResponseHandle<T>,
+    {
+        match self {
+            ResponseError::Recoverable(e) => ResponseError::Recoverable(e.map_handle(f)),
+            ResponseError::Fatal(e) => ResponseError::Fatal(e),
+        }
+    }
+}
+
+// have to do it manually, somehow derive fucks it up
+impl<H: ResponseHandle<T>, T> Debug for ResponseError<H, T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ResponseError::Recoverable(e) => Debug::fmt(e, f),
+            ResponseError::Fatal(e) => Debug::fmt(e, f),
+        }
+    }
+}
+
+// TODO maybe some sort of ignore method for Result<T, ResponseError> that loops
+// until it succeeds or a fatal error occurs
+
+//
+
 /// Handle to wait for a motor response
 pub trait ResponseHandle<T> {
-    // TODO maybe return handle again on some errors that are still recoverable
     /// Check the response for correctness and return the result contained in the
     /// response, if there was one. Usually only getters have a returnvalue. If
     /// the response only needs to be checked for correctness but no value is
@@ -68,11 +155,14 @@ pub trait ResponseHandle<T> {
     /// handle2.wait().unwrap();
     /// println!("motors stopped");
     /// ```
-    fn wait(self) -> Result<T, DriverError>;
+    fn wait(self) -> Result<T, ResponseError<Self, T>>
+    where
+        Self: Sized;
 }
 
 //
 
+#[derive(Debug)]
 pub(super) struct ReadResponseHandle<I: Write + Read, T, P>
 where
     P: Fn(&[u8]) -> Result<T, DriverError>,
@@ -107,10 +197,24 @@ impl<I: Write + Read, T, P> ResponseHandle<T> for ReadResponseHandle<I, T, P>
 where
     P: Fn(&[u8]) -> Result<T, DriverError>,
 {
-    fn wait(self) -> Result<T, DriverError> {
+    // the whole match error and drop shenannigans are needed to statisfy the
+    // borrow checker
+    fn wait(self) -> Result<T, ResponseError<Self, T>> {
         let mut driver = self.driver.as_ref().borrow_mut();
-        let payload = driver.receive_single(self.address)?;
-        (self.parser)(&payload)
+        let payload = match driver.receive_single(self.address) {
+            Ok(p) => p,
+            Err(e) => {
+                drop(driver);
+                return Err(ResponseError::from_driver_error(self, e));
+            }
+        };
+        match (self.parser)(&payload) {
+            Ok(p) => Ok(p),
+            Err(e) => {
+                drop(driver);
+                Err(ResponseError::from_driver_error(self, e))
+            }
+        }
     }
 }
 
@@ -120,6 +224,7 @@ where
 // implementations for read and write were split so we don't need to parse as much
 // since for write commands we only need to check if the payload matched what we
 // sent and it also makes WriteResponseHandle::wait a bit faster
+#[derive(Debug)]
 pub(super) struct WriteResponseHandle<I: Write + Read> {
     driver: Rc<RefCell<InnerDriver<I>>>,
     address: MotorAddress,
@@ -138,16 +243,29 @@ impl<I: Write + Read> WriteResponseHandle<I> {
 }
 
 impl<I: Write + Read> ResponseHandle<()> for WriteResponseHandle<I> {
-    fn wait(self) -> Result<(), DriverError> {
+    // the whole match error and drop shenannigans are needed to statisfy the
+    // borrow checker
+    fn wait(self) -> Result<(), ResponseError<Self, ()>> {
         let mut driver = self.driver.as_ref().borrow_mut();
         let payload = match self.address {
-            MotorAddress::Single(a) => driver.receive_single(a)?,
-            MotorAddress::All => driver.receive_all()?,
+            MotorAddress::Single(a) => driver.receive_single(a),
+            MotorAddress::All => driver.receive_all(),
+        };
+        let payload = match payload {
+            Ok(p) => p,
+            Err(e) => {
+                drop(driver);
+                return Err(ResponseError::from_driver_error(self, e));
+            }
         };
         if self.sent == payload {
             Ok(())
         } else {
-            Err(DriverError::NonMatchingPayloads(payload))
+            drop(driver);
+            Err(ResponseError::from_driver_error(
+                self,
+                DriverError::NonMatchingPayloads(payload),
+            ))
         }
     }
 }
@@ -155,6 +273,7 @@ impl<I: Write + Read> ResponseHandle<()> for WriteResponseHandle<I> {
 //
 
 // Responsehandle for motors with RespondMode::Quiet
+#[derive(Debug)]
 pub(super) struct DummyResponseHandle();
 
 impl DummyResponseHandle {
@@ -164,7 +283,7 @@ impl DummyResponseHandle {
 }
 
 impl ResponseHandle<()> for DummyResponseHandle {
-    fn wait(self) -> Result<(), DriverError> {
+    fn wait(self) -> Result<(), ResponseError<Self, ()>> {
         Ok(())
     }
 }
@@ -173,17 +292,20 @@ impl ResponseHandle<()> for DummyResponseHandle {
 
 // Wrapper so we can return Write and Dummy, read is not needed since read
 // commands always return a value
+#[derive(Debug)]
 pub(super) enum WrapperResponseHandle<I: Read + Write> {
     Write(WriteResponseHandle<I>),
     Dummy(DummyResponseHandle),
 }
 
 impl<I: Write + Read> ResponseHandle<()> for WrapperResponseHandle<I> {
-    fn wait(self) -> Result<(), DriverError> {
+    fn wait(self) -> Result<(), ResponseError<Self, ()>> {
         use WrapperResponseHandle::*;
+        // the maps are required to map from ResponseError<WriteResponseHandle> or
+        // ResponseError<DummyResponseHandle> to ResponseError<WrapperResponseHandle>
         match self {
-            Write(h) => h.wait(),
-            Dummy(h) => h.wait(),
+            Write(h) => h.wait().map_err(|e| e.map_handle(|h| Write(h))),
+            Dummy(h) => h.wait().map_err(|e| e.map_handle(|h| Dummy(h))),
         }
     }
 }
