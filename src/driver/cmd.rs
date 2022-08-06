ed8982619198016a0756fa88f553843e3fdc859b
Maximilian Fischer <mfischer@ps-3d.de>
2022-08-06 16:18:49 +0200

    fix docs warning

--- src/driver/cmd.rs
+++ src/driver/cmd.rs
@@ -1,7 +1,7 @@
 //! Provides types for the raw commands of the driver.
 //!
 //! [`frame`] is responsible for everything except the payload, e.g. the message
-//! as a whole or the [`MotorAddress`][frame::MotorAdrress].
+//! as a whole or the [`MotorAddress`][frame::MotorAddress].
 //! [`payload`] is responsible for all the types needed for the various payloads.
 
 pub mod frame;
